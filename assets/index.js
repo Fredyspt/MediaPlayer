336d98f2505897bfac099a3718fc2f3edc4bb3d5
Fredyspt <fredyfps@gmail.com>
2021-05-07 09:24:25 -0600

    Added a few examples regarding this
    Signed-off-by: Fredyspt <fredyfps@gmail.com>

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,9 @@ import AutoPlay from './plugins/AutoPlay.js'
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 const muteButton = document.getElementById("mute")
-const player = new MediaPlayer({element: video, plugins: [new AutoPlay()]});
+const player = new MediaPlayer({element: video, plugins: [
+    // new AutoPlay()
+]});
 
 button.onclick = () => player.togglePlay();
 muteButton.onclick = () => player.toggleMute();
